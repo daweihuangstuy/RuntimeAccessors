45a770554f79296f4ecca39884e747cee3113714
Karina Ionkina <Karina@Karinas-Air.home>
2017-02-14 20:46:23 -0500

    genrev creates arrays in descending order. we can use them for sorting?

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -18,18 +18,31 @@
   ANALYSIS:
   <INSERT YOUR RESULTS ANALYSIS HERE>
   ======================================*/
-
+import java.util.Arrays;
 public class MergeSortTester 
 {
-
+    //returns backwards int[] array of n integers 
+    // ie: genrev(10) = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+    public static int[] genrev(int n){
+	int[] bob = new int[n];
+	for (int i = n - 1; i > 0; i--){
+	    bob[n - 1- i] = i;
+	}
+	return bob;
+    }
+	
+    
     /******************************
      * execution time analysis 
-     * <INSERT YOUR DESCRIPTION HERE OF 
+     * Runtime accessed with System.currentTimeMillis() 
      *  YOUR APPARATUS FOR GENERATING EXECUTION 
      *  TIME DATA...>
      ******************************/
     public static void main( String[] args ) 
     {
+	// System.out.println(Arrays.toString(genrev(10)));
+	long start = System.currentTimeMillis();
+	
 
     }//end main
 
