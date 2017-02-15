d114ea15ec972bf25d8038bdb41fb7c3e66cfcdb
Shaikat Islam <sislam9@stuy.edu>
2017-02-14 23:01:53 -0500

    added test cases

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -60,12 +60,52 @@ public class MergeSortTester
     {
 
 	//long from access divided by number of arrays for average runTime
-	int[][] cs = nUnsorted(10000, 16);
-	int[][] cs2 = nUnsorted(100000, 16);
-	double tim = access(cs) / 10000.0;
-	double george = access(cs2) / 100000.0;
-	System.out.println("Approximate time to sort an array of 16 elements: " + tim + " milliseconds.");
-	System.out.println("Approximate time to sort an array of 16 elements: " + george + " milliseconds.");
+	//long from access divided by number of arrays for average runTime
+	int[][] cs2 = nUnsorted(10000000, 2);
+	double george = access(cs2) / 10000000.0;
+	System.out.println("Approximate time to sort an array of 2 elements: " + george + " milliseconds.");
+	int[][] cs3 = nUnsorted(10000000, 4);
+	double david = access(cs3) / 10000000.0;	
+	System.out.println("Approximate time to sort an array of 4 elements: " + david + " milliseconds.");
+	int[][] cs4 = nUnsorted(10000000, 8);
+	double john = access(cs4) / 10000000.0;	
+	System.out.println("Approximate time to sort an array of 8 elements: " + john + " milliseconds.");
+	int[][] cs5 = nUnsorted(10000000, 16);
+	double ringo = access(cs5) / 10000000.0;	
+	System.out.println("Approximate time to sort an array of 16 elements: " + ringo + " milliseconds.");
+	int[][] cs6 = nUnsorted(10000000, 10);
+	double paul = access(cs3) / 10000000.0;	
+	System.out.println("Approximate time to sort an array of 10 elements: " + paul + " milliseconds.");
+	int[][] cs7 = nUnsorted(100000, 32);
+	double elvis = access(cs7) / 100000.0;	
+	System.out.println("Approximate time to sort an array of 32 elements: " + elvis + " milliseconds.");
+	int[][] cs8 = nUnsorted(100000, 64);
+	double bruno = access(cs8) / 100000.0;	
+	System.out.println("Approximate time to sort an array of 64 elements: " + bruno + " milliseconds.");
+	int[][] cs9 = nUnsorted(100000, 128);
+	double lex = access(cs9) / 100000.0;	
+	System.out.println("Approximate time to sort an array of 128 elements: " + lex + " milliseconds.");
+	int[][] cs10 = nUnsorted(100000, 100);
+	double clark = access(cs10) / 100000.0;	
+	System.out.println("Approximate time to sort an array of 100 elements: " + clark + " milliseconds.");
+	int[][] cs11 = nUnsorted(10000, 1000);
+	double bruce = access(cs11) / 10000.0;	
+	System.out.println("Approximate time to sort an array of 1000 elements: " + bruce + " milliseconds.");
+	int[][] cs12 = nUnsorted(100, 100000);
+	double doomsday = access(cs12) / 100.0;	
+	System.out.println("Approximate time to sort an array of 100000 elements: " + doomsday + " milliseconds.");
+	int[][] cs13 = nUnsorted(1000, 500);
+	double doomsday1 = access(cs13) / 1000.0;	
+	System.out.println("Approximate time to sort an array of 500 elements: " + doomsday1 + " milliseconds.");
+	int[][] cs14 = nUnsorted(1000, 750);
+	double doomsday2 = access(cs14) / 1000.0;	
+	System.out.println("Approximate time to sort an array of 750 elements: " + doomsday2 + " milliseconds.");
+	int[][] cs15 = nUnsorted(100, 50000);
+	double doomsday3 = access(cs15) / 100.0;	
+	System.out.println("Approximate time to sort an array of 50000 elements: " + doomsday3 + " milliseconds.");
+	int[][] cs16 = nUnsorted(100, 25000);
+	double doomsday4 = access(cs15) / 100.0;	
+	System.out.println("Approximate time to sort an array of 25000 elements: " + doomsday4 + " milliseconds.");
 	
 
        
